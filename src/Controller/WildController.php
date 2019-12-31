551866e1ccaad67e6bc15c6f54864c1cf674cac4
Marie Marie <Mariedefre87@gmail.com>
2019-12-31 12:49:59 +0100

    quetes_05 = Le routing avanc&é

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,16 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 Class WildController extends AbstractController
 {
     /**
      * @Route("/wild", name="wild_index")
      */
+
     public function index() : Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+
+    /**
+     * @Route("/wild/show/{page<[a-z0-9-]+$>}", name="wild_show", defaults={"page"="Aucune série sélectionnée, veuillez choisir une série"})
+     * @param string $page
+     * @return Response
+     */
+
+    public function show(string $page): Response
+    {
+        $page = str_replace('-', ' ', $page);
+        $page = ucwords($page);
+        return $this->render('wild/show.html.twig', ['page' => $page]);
     }
 }
 
